3037d00209c580d5fcb50af7fab55d60d258d0e3
jagadeshwaran <jagsartchannel@gmail.com>
2026-09-17 18:23:29 +0530

    Added: agent tool layer

---
+++ tools/agent_tools.py
@@ -0,0 +1,19 @@
+from langchain_core.tools import tool
+from tools.file_tools import *
+
+
+@tool
+def read_file_tool(file_path: str) -> str:
+    """Read the contents of a file."""
+    return read_file(file_path)
+
+
+@tool
+def write_file_tool(file_path: str,content: str) -> str:
+    """Write content to a file."""
+    return write_file(file_path, content)
+
+@tool
+def list_files_tool(directory: str) -> list[str]:
+    """List files in a directory."""
+    return list_files(directory)
